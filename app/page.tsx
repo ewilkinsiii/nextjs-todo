bec9b1e49730c1c2b853367cbd3da4f4c2cb63bc
ewilkinsiii <ewilkinsiii@gmail.com>
2023-07-01 00:08:35 -0500

    added task button

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
+import AddTask from "./components/AddTask";
 export default function Home() {
   return (
     <main className="max-w-4xl mx-auto mt-4">
       <div className="flex flex-col gap-4 my-5 text-center">
         <h1 className="text-2xl font-bold">Todo List</h1>
+        <AddTask />
       </div>
     </main>
   );
