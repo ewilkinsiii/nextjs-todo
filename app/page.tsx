bc8a6ea97ba5c0773f1c4e7386a1a0feff564273
ewilkinsiii <ewilkinsiii@gmail.com>
2023-07-01 16:03:49 -0500

    added modal

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,10 @@
+//"use client";
 import { getAllTodos } from "@/api";
 import AddTask from "./components/AddTask";
 import TodoList from "./components/TodoList";
 
 export default async function Home() {
   const tasks = await getAllTodos();
-  console.log(tasks);
 
   return (
     <main className="max-w-4xl mx-auto mt-4">
